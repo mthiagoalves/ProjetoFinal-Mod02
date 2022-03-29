2c0b5ca868b54ac9725dca854f5edc22bee433c4
mthiagoalves <m.thiago.alves@live.com>
2022-03-29 20:33:25 -0300

    andamanto

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(session({secret: 'user157'}));
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
+let login = "username";
+let password = "123456";
+
+app.post("/", (req,res) => {
+    if(req.body.password == password && req.body.login == login){
+       req.session.login = login;
+
+       res.render("home");
+
+    } else{
+
+        res.render("index");
+    }
+    
+});
 
 app.get("/", (req, res) => {
-    res.render("index");
+    if(req.session.login){
+        res.render("home")
+    } else{
+        res.render("index");
+    }
+    
 });
+
+
+app.post("/cadastro", (req,res) => {
+    res.render("cadastro");
+});
+
+app.post("/detalhes", (req,res) => {
+    res.render("/detalhes")
+})
 
 app.listen(port, ()=> {
     console.log(`Servidor rodando na URL: http://localhost:${port}`)
